888efe4b07dcf740fd77f6c74f0c012d26ef4575
Juri Kaemper <s210922@th-ab.de>
2022-07-21 19:35:45 +0200

    cell size can now be adjusted

--- Cell.java
+++ Cell.java
@@ -3,21 +3,26 @@ import java.awt.Rectangle;
 public class Cell extends Rectangle {
     private boolean alive = false;
     private boolean nextGenAlive = false;
-    private static int width = 10;
-    private static int height = 10;
+
+    private static int cellWidth = 3;
+    private static int cellHeight = 3;
 
     private int neighbours = 0;
 
-    private static Cell[][] cells = Main.getCells();
+    private static Cell[][] cells;
 
 
     public Cell(int i, int j) {
-        this.setBounds(10 * i, 10 * j, width, height);
+        this.setBounds(cellWidth * i, cellHeight * j, cellWidth, cellHeight);
     }
     
     public static void initializeCells() {
-        for (int i = 0; i < 50; i++) {
-            for (int j = 0; j < 50; j++) {
+        int xGrids = Main.getPanelWidth() / cellWidth;
+        int yGrids = Main.getPanelHeight() / cellHeight;
+        cells = new Cell[xGrids][yGrids];
+
+        for (int i = 0; i < xGrids; i++) {
+            for (int j = 0; j < yGrids; j++) {
                 cells[i][j] = new Cell(i, j);
             }
         }
@@ -45,5 +50,29 @@ public class Cell extends Rectangle {
 
     public void setNextGenAlive(boolean nextGenAlive) {
         this.nextGenAlive = nextGenAlive;
+    }
+
+    public static int getCellWidth() {
+        return cellWidth;
+    }
+
+    public static void setCellWidth(int cellWidth) {
+        Cell.cellWidth = cellWidth;
+    }
+
+    public static int getCellHeight() {
+        return cellHeight;
+    }
+
+    public static void setCellHeight(int cellHeight) {
+        Cell.cellHeight = cellHeight;
+    }
+
+    public static Cell[][] getCells() {
+        return cells;
+    }
+
+    public static void setCells(Cell[][] cells) {
+        Cell.cells = cells;
     }
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
@@ -7,34 +9,46 @@ import javax.swing.JPanel;
 public class Main extends JPanel {
 
     private static JFrame frame;
+    private static JPanel topPanel;
 
-    private static int width = 516;
-    private static int height = 539;
+    private static int panelWidth = 500;
+    private static int panelHeight = 500;
+
+    private static int width = panelWidth + 16;
+    private static int height = panelHeight + 39 + 75;
+
+    private static int xGrids;
+    private static int yGrids;
 
     private static boolean initialized;
-
-    private static Cell[][] cells = new Cell[500 / 10][500 / 10];
 
     public Main() {
         frame = new JFrame();
         frame.setTitle("Game of Life");
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
 
-        this.setSize(500, 500);
+        this.setPreferredSize(new Dimension(panelWidth, panelHeight));
         this.setBackground(Color.BLACK);
 
-        frame.add(this);
+        topPanel = new JPanel();
+        topPanel.setBackground(Color.DARK_GRAY);
+        topPanel.setPreferredSize(new Dimension(0, 75));
+
+        frame.add(this, BorderLayout.CENTER);
+        frame.add(topPanel, BorderLayout.NORTH);
 
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
         this.repaint();
     }
 
     public static void main(String[] args) {
         Main main = new Main();
         Cell.initializeCells();
+        xGrids = panelWidth / Cell.getCellWidth();
+        yGrids = panelHeight / Cell.getCellHeight();
 
         Thread t1 = new Thread(new Runnable() {
 
@@ -42,14 +56,14 @@ public class Main extends JPanel {
             public void run() {
                 while (true) {
                     try {
-                        Thread.sleep(500);
+                        Thread.sleep(30);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
 
-                    for (int i = 1; i < 49; i++) {
-                        for (int j = 1; j < 49; j++) {
-                            Cell currentCell = cells[i][j];
+                    for (int i = 1; i < xGrids - 1; i++) {
+                        for (int j = 1; j < yGrids - 1; j++) {
+                            Cell currentCell = Cell.getCells()[i][j];
                             countNeighbours(currentCell, i, j);
                         }
                     }
@@ -68,7 +82,7 @@ public class Main extends JPanel {
         // counts alive neighbours of every single cell
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
-                if (cells[xGrid + i][yGrid + j].isAlive())
+                if (Cell.getCells()[xGrid + i][yGrid + j].isAlive())
                     if (i != 0 || j != 0) {
                         neighbours++;
                     }
@@ -90,17 +104,17 @@ public class Main extends JPanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.setColor(Color.WHITE);
-
+        
         // draw first alive cells
         if (!initialized) {
-            for (int i = 1; i < 49; i++) {
-                for (int j = 1; j < 49; j++) {
-                    if (j % 10 == 0 || i % 10 == 0) {
-                        int x = (int) (cells[i][j].getX());
-                        int y = (int) (cells[i][j].getY());
+            g.setColor(Color.WHITE);
+            for (int i = 1; i < xGrids - 1; i++) {
+                for (int j = 1; j < yGrids - 1; j++) {
+                    if (j % 7 == 0 || i % 7 == 0) {
+                        int x = (int) (Cell.getCells()[i][j].getX());
+                        int y = (int) (Cell.getCells()[i][j].getY());
                         g.fillRect(x, y, 10, 10);
-                        cells[i][j].setNextGenAlive(true);
+                        Cell.getCells()[i][j].setNextGenAlive(true);
                     }
                 }
             }
@@ -108,24 +122,36 @@ public class Main extends JPanel {
         }
 
         // draw cells
-        for (int i = 1; i < 49; i++) {
-            for (int j = 1; j < 49; j++) {
-                int x = (int) (cells[i][j].getX());
-                int y = (int) (cells[i][j].getY());
-                if (cells[i][j].isNextGenAlive()) {
+        for (int i = 1; i < xGrids - 1; i++) {
+            for (int j = 1; j < yGrids - 1; j++) {
+                int x = (int) (Cell.getCells()[i][j].getX());
+                int y = (int) (Cell.getCells()[i][j].getY());
+                if (Cell.getCells()[i][j].isNextGenAlive()) {
                     g.setColor(Color.WHITE);
-                    cells[i][j].setAlive(true);
+                    Cell.getCells()[i][j].setAlive(true);
                 } else {
                     g.setColor(Color.BLACK);
-                    cells[i][j].setAlive(false);
+                    Cell.getCells()[i][j].setAlive(false);
                 }
-                g.fillRect(x, y, 10, 10);
+                g.fillRect(x, y, Cell.getCellWidth(), Cell.getCellHeight());
             }
         }
 
     }
 
-    public static Cell[][] getCells() {
-        return cells;
+    public static int getPanelWidth() {
+        return panelWidth;
+    }
+
+    public static int getPanelHeight() {
+        return panelHeight;
+    }
+
+    public static int getxGrids() {
+        return xGrids;
+    }
+
+    public static int getyGrids() {
+        return yGrids;
     }
 }
